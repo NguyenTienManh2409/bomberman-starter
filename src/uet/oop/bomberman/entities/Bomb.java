169c266d4093ba23ba38dd2c34028f98f20a9c9c
NamHuyNguyenKhac <21020542@vnu.edu.vn>
2022-10-24 01:13:11 +0700

    Tao ra Flame nhung k chay dc game tu luc fetch file ve, create tam thoi 4 method left right up down o class bomb thay vi flamedirect do k check dc va cham

--- src/uet/oop/bomberman/entities/Bomb.java
+++ src/uet/oop/bomberman/entities/Bomb.java
@@ -3,14 +3,25 @@ package uet.oop.bomberman.entities;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.entities.FlameDirect;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bomb extends Entity {
     int animate = 0;
     private long _timeToExplode;
+    public static int LENGTH_OF_FLAME = 1;
     public static final int TIME_TO_EXPLOSION_BOMB = 300;
     public static final int TIME_TO_DISAPPEAR = 100;
     private boolean _exploding = false;
     private boolean _destroyed = false;
+
+    private final List<Flame> flameList = new ArrayList<>();
+
+    public List<Flame> getFlameList() {
+        return flameList;
+    }
     public Bomb(double xUnit, double yUnit, Image img) {
         super(xUnit, yUnit, img);
     }
@@ -39,6 +50,100 @@ public class Bomb extends Entity {
                 }
             }
         }
+    }
+
+    private void initAndHandleFlame() {
+        flameList.clear();
+        Flame newFlame = null;
+        for (int i = 0; i < Bomb.LENGTH_OF_FLAME; i++) {
+            if (!leftable(x - i * Sprite.SCALED_SIZE, y)) {
+                break;
+            }
+            newFlame =
+                    new Flame(
+                            (x - (1 + i) * Sprite.SCALED_SIZE) / Sprite.SCALED_SIZE,
+                            y / Sprite.SCALED_SIZE,
+                            Sprite.explosion_horizontal.getFxImage());
+
+            if (i != Bomb.LENGTH_OF_FLAME - 1) {
+                newFlame.setStatus("HORIZONTAL");
+            } else {
+                newFlame.setStatus("LEFT_LAST");
+            }
+            flameList.add(newFlame);
+        }
+
+        for (int i = 0; i < Bomb.LENGTH_OF_FLAME; i++) {
+            if (!rightable(x + i * Sprite.SCALED_SIZE, y)) {
+                break;
+            }
+            newFlame =
+                    new Flame(
+                            (x + (1 + i) * Sprite.SCALED_SIZE) / Sprite.SCALED_SIZE,
+                            y / Sprite.SCALED_SIZE,
+                            Sprite.explosion_horizontal.getFxImage());
+            if (i != Bomb.LENGTH_OF_FLAME - 1) {
+                newFlame.setStatus("HORIZONTAL");
+            } else {
+                newFlame.setStatus("RIGHT_LAST");
+            }
+            flameList.add(newFlame);
+        }
+
+        for (int i = 0; i < Bomb.LENGTH_OF_FLAME; i++) {
+            if (!upable(x, y - i * Sprite.SCALED_SIZE)) {
+                break;
+            }
+            newFlame =
+                    new Flame(
+                            x / Sprite.SCALED_SIZE,
+                            (y - (1 + i) * Sprite.SCALED_SIZE) / Sprite.SCALED_SIZE,
+                            Sprite.explosion_vertical.getFxImage());
+            if (i != Bomb.LENGTH_OF_FLAME - 1) {
+                newFlame.setStatus("VERTICAL");
+            } else {
+                newFlame.setStatus("TOP_LAST");
+            }
+            flameList.add(newFlame);
+        }
+
+        for (int i = 0; i < Bomb.LENGTH_OF_FLAME; i++) {
+            if (!downable(x, y + i * Sprite.SCALED_SIZE)) {
+                break;
+            }
+            newFlame =
+                    new Flame(
+                            x / Sprite.SCALED_SIZE,
+                            (y + (1 + i) * Sprite.SCALED_SIZE) / Sprite.SCALED_SIZE,
+                            Sprite.explosion_vertical.getFxImage());
+            if (i != Bomb.LENGTH_OF_FLAME - 1) {
+                newFlame.setStatus("VERTICAL");
+            } else {
+                newFlame.setStatus("DOWN_LAST");
+            }
+            flameList.add(newFlame);
+        }
+        newFlame =
+                new Flame(
+                        x / Sprite.SCALED_SIZE, y / Sprite.SCALED_SIZE, Sprite.explosion_vertical.getFxImage());
+        newFlame.setStatus("CENTER");
+        flameList.add(newFlame);
+    }
+
+    private boolean downable(double x, double v) {
+        return true;
+    }
+
+    private boolean upable(double x, double v) {
+        return true;
+    }
+
+    private boolean rightable(double v, double y) {
+        return true;
+    }
+
+    private boolean leftable(double v, double y) {
+        return true;
     }
 
     public boolean isDestroyed() {
